39e181434c74e55d0391af74179253fbb9c029fc
KyHemming <kyhemming@gmail.com>
2023-09-23 11:48:46 +0100

    Bullet Cooldown

--- Stage.cs
+++ Stage.cs
@@ -7,6 +7,8 @@ public class Stage : Node
 	private Vector2 ScreenSize;
 	private Player player;
 	private int bulletSpeed = 500;
+	private float BulletCooldown = 0.5f;
+	private float CurrentBulletCooldown = 0f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,28 +23,36 @@ public class Stage : Node
 	public override void _Process(float delta)
 	{
 		Bullet bullet = (Bullet)BulletScene.Instance();
-		if (Input.IsActionJustPressed("shoot_up"))
+		if (CurrentBulletCooldown <= 0) {
+			GetBulletVelocity(bullet);
+			if (bullet.velocity != Vector2.Zero) {
+				this.AddChild(bullet);
+				CurrentBulletCooldown = BulletCooldown;
+			}
+		}
+		CurrentBulletCooldown = Mathf.Max(0, CurrentBulletCooldown - delta);
+	}
+	
+	private void GetBulletVelocity(Bullet bullet) {
+		if (Input.IsActionPressed("shoot_up"))
 		{
 			bullet.Position = player.Position;
 			bullet.velocity.y -= 1;
 		}
-		if (Input.IsActionJustPressed("shoot_down"))
+		if (Input.IsActionPressed("shoot_down"))
 		{
 			bullet.Position = player.Position;
 			bullet.velocity.y += 1;
 		}
-		if (Input.IsActionJustPressed("shoot_left"))
+		if (Input.IsActionPressed("shoot_left"))
 		{
 			bullet.Position = player.Position;
 			bullet.velocity.x -= 1;
 		}
-		if (Input.IsActionJustPressed("shoot_right"))
+		if (Input.IsActionPressed("shoot_right"))
 		{
 			bullet.Position = player.Position;
 			bullet.velocity.x += 1;
-		}
-		if (bullet.velocity != Vector2.Zero) {
-			this.AddChild(bullet);
 		}
 	}
 }
