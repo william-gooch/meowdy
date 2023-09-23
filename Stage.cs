e7916eb7ea0f5dfcc6b4c34c4c2c34930da6bed0
esh1 <esh1@pc7-089-l.cs.st-andrews.ac.uk>
2023-09-23 12:29:52 +0100

    Spawning in mobs

--- Stage.cs
+++ Stage.cs
@@ -4,24 +4,38 @@ using System;
 public class Stage : Node
 {
 	private PackedScene BulletScene;
+	private PackedScene RatScene;
 	private Vector2 ScreenSize;
 	private Player player;
 	private int bulletSpeed = 500;
 	private float BulletCooldown = 0.5f;
 	private float CurrentBulletCooldown = 0f;
+	private float MobTimer;
+	private Random rnd;
+	private Position2D LeftSpawnPosition;
+	private Position2D RightSpawnPosition;
+	private Position2D TopSpawnPosition;
+	private Position2D BottomSpawnPosition;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		BulletScene = GD.Load<PackedScene>("res://Bullet.tscn");
-		GD.Randomize();
-		player = GetNode<Player>("Player");
+		RatScene = GD.Load<PackedScene>("res://Rat.tscn");
 		var startPosition = GetNode<Position2D>("StartPosition");
+		LeftSpawnPosition = GetNode<Position2D>("LeftSpawnPosition");
+		RightSpawnPosition = GetNode<Position2D>("RightSpawnPosition");
+		TopSpawnPosition = GetNode<Position2D>("TopSpawnPosition");
+		BottomSpawnPosition = GetNode<Position2D>("BottomSpawnPosition");
+		GD.Randomize();
+		rnd = new Random();
+		player = GetNode<Player>("Player");
 		player.Start(startPosition.Position);
+		MobTimer = 3f;
 	}
 	
 	public override void _Process(float delta)
 	{
+		//Bullet Shooting Physics
 		Bullet bullet = (Bullet)BulletScene.Instance();
 		if (CurrentBulletCooldown <= 0) {
 			GetBulletVelocity(bullet);
@@ -30,9 +44,38 @@ public class Stage : Node
 				CurrentBulletCooldown = BulletCooldown;
 			}
 		}
+		
+		//Spawning Mobs in 4 locations every 3 seconds
+		if (MobTimer <= 0) {
+			Rat rat = (Rat)RatScene.Instance();
+			int location = rnd.Next(1,5);
+			GD.Print(location);
+			switch(location) {
+				case 1:
+					rat.Position = LeftSpawnPosition.Position;
+					break;
+				case 2:
+					rat.Position = RightSpawnPosition.Position;
+					break;
+				case 3:
+					rat.Position = TopSpawnPosition.Position;
+					break;
+				case 4:
+					rat.Position = BottomSpawnPosition.Position;
+					break;
+				default:
+					break;
+			}
+			this.AddChild(rat);
+			MobTimer = 3f; //TODO: Reduce time as time goes on
+		}
+		
+		// Deduct time from timers and cooldowns
 		CurrentBulletCooldown = Mathf.Max(0, CurrentBulletCooldown - delta);
+		MobTimer = Mathf.Max(0, MobTimer - delta);
 	}
 	
+	//Get Bullet Velocity from inputs
 	private void GetBulletVelocity(Bullet bullet) {
 		if (Input.IsActionPressed("shoot_up"))
 		{
