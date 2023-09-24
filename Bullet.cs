7551848b5905ed7669a1c1f4fb97a17851b94432
William <william.gooch.wg@gmail.com>
2023-09-24 15:08:44 +0100

    FIx memory leak uwu

--- Bullet.cs
+++ Bullet.cs
@@ -16,12 +16,16 @@ public class Bullet : Area2D
 
 	public override void _Process(float delta)
 	{
-		if (Destroyed) {
-			if (deathTime <= 0) {
+		if (Destroyed)
+		{
+			if (deathTime <= 0)
+			{
 				this.QueueFree();
 			}
 			deathTime = Mathf.Max(0, deathTime - delta);
-		} else {
+		}
+		else
+		{
 			direction = direction.Normalized();
 			Position += direction * Speed * (float)delta;
 			Position = new Vector2(Position.x, Position.y);
@@ -29,18 +33,22 @@ public class Bullet : Area2D
 	}
 	private async void _on_Bullet_area_entered(object area)
 	{
-		if (area is Enemy | area is BigObstacle | area is Obstacle) {
-			if(area is Enemy) {
+		if (area is Enemy | area is BigObstacle | area is Obstacle)
+		{
+			if (area is Enemy)
+			{
 				(area as Enemy).EmitSignal(nameof(Enemy.HitEventHandler), this.direction);
 			}
 
 			CPUParticles2D DeathParticles = GetNode<CPUParticles2D>("DeathParticles");
 			await ToSignal(GetTree().CreateTimer(0.01f), "timeout");
-			if(HasNode("BulletSprite")) {
-				RemoveChild(GetNode<Sprite>("BulletSprite"));
+			if (HasNode("BulletSprite"))
+			{
+				GetNode<Sprite>("BulletSprite").QueueFree();
 			}
-			if(HasNode("CollisionShape2D")) {
-				RemoveChild(GetNode<CollisionShape2D>("CollisionShape2D"));
+			if (HasNode("CollisionShape2D"))
+			{
+				GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
 			}
 			DeathParticles.Show();
 			Destroyed = true;
--- Stage.cs
+++ Stage.cs
@@ -7,32 +7,32 @@ public class Stage : Node
 	private PackedScene RatScene;
 	private PackedScene BigRatScene;
 	private PackedScene MilkScene = GD.Load<PackedScene>("res://Milk.tscn");
-	private PackedScene CatnipScene= GD.Load<PackedScene>("res://Catnip.tscn");
+	private PackedScene CatnipScene = GD.Load<PackedScene>("res://Catnip.tscn");
 	private TileMap Background;
 	private Vector2 ScreenSize;
 	private Player player;
 	private HUD HUD;
-	
+
 	//Bullet Physics
-	public float CHARGE_SHOT_COOLDOWN{get; set;} = 3f; // Press "F" key for charge shot
+	public float CHARGE_SHOT_COOLDOWN { get; set; } = 3f; // Press "F" key for charge shot
 	private float BulletCooldown = 0.5f;
 	private float CurrentBulletCooldown = 0f;
 	private float CurrentChargeShotCooldown = 0f;
-	
+
 	//Mob Timer
 	private float MOB_TIME = 2f;
 	private float MobTimer;
 	private float BigRatSpawnChance = 0.9f;
-	
+
 	//Mob Spawn points
 	private Random rnd;
 	private Position2D LeftSpawnPosition;
 	private Position2D RightSpawnPosition;
 	private Position2D TopSpawnPosition;
 	private Position2D BottomSpawnPosition;
-	
+
 	private int NUM_OBSTACLES = 6;
-	
+
 	//Power up
 	private float PowerUpCooldown = 10f;
 	private float CurrentPowerUpCooldown;
@@ -53,108 +53,127 @@ public class Stage : Node
 		BottomSpawnPosition = GetNode<Position2D>("BottomSpawnPosition");
 		Background = GetNode<TileMap>("Background");
 		ScreenSize = GetViewport().Size;
-		TODO: CurrentPowerUpCooldown = PowerUpCooldown;
+	TODO: CurrentPowerUpCooldown = PowerUpCooldown;
 		GD.Randomize();
 		rnd = new Random();
 		player = GetNode<Player>("Player");
 		player.Start(startPosition.Position);
 		MobTimer = 3f;
-		
+
 		// Spawn Obstacles
 		// TODO: Prevent spawn on player or give i frames at start
-		for (int i = 0; i < NUM_OBSTACLES; i++) {
+		for (int i = 0; i < NUM_OBSTACLES; i++)
+		{
 			Area2D obstacle;
-			if (rnd.Next(1,5) == 1) {
+			if (rnd.Next(1, 5) == 1)
+			{
 				obstacle = (BigObstacle)BigObstacleScene.Instance();
-			} else {
+			}
+			else
+			{
 				obstacle = (Obstacle)ObstacleScene.Instance();
 			}
 			obstacle.Position = new Vector2(
-				rnd.Next(0,(int)ScreenSize.x),
-				rnd.Next(0,(int)ScreenSize.y)
+				rnd.Next(0, (int)ScreenSize.x),
+				rnd.Next(0, (int)ScreenSize.y)
 			);
-			this.AddChildBelowNode(Background,obstacle);
+			this.AddChildBelowNode(Background, obstacle);
 		}
 	}
-	
+
 	public override void _Process(float delta)
 	{
 		//Bullet Shooting Physics
-		if (CurrentBulletCooldown <= 0) {
-			if (ShootFromMovement()) {
+		if (CurrentBulletCooldown <= 0)
+		{
+			if (ShootFromMovement())
+			{
 				CurrentBulletCooldown = BulletCooldown;
 			}
 		}
-		
+
 		//MobSpawner
-		if (MobTimer <= 0) {
+		if (MobTimer <= 0)
+		{
 			SpawnMob();
 		}
-		
+
 		// Charge Shot Physics
-		if (CurrentChargeShotCooldown <= 0f & Input.IsActionPressed("charge_shot")) {
+		if (CurrentChargeShotCooldown <= 0f & Input.IsActionPressed("charge_shot"))
+		{
 			ShootUp((Bullet)BulletScene.Instance());
 			ShootDown((Bullet)BulletScene.Instance());
 			ShootLeft((Bullet)BulletScene.Instance());
 			ShootRight((Bullet)BulletScene.Instance());
 			CurrentChargeShotCooldown = CHARGE_SHOT_COOLDOWN;
 		}
-		
+
 		//Power Up Spawner
-		if (CurrentPowerUpCooldown <= 0) {
+		if (CurrentPowerUpCooldown <= 0)
+		{
 			SpawnPowerUp();
 			CurrentPowerUpCooldown = PowerUpCooldown;
 		}
-		
-		
+
+
 		// Deduct time from timers and cooldowns
 		CurrentChargeShotCooldown = Mathf.Max(0, CurrentChargeShotCooldown - delta);
 		CurrentBulletCooldown = Mathf.Max(0, CurrentBulletCooldown - delta);
 		CurrentPowerUpCooldown = Mathf.Max(0, CurrentPowerUpCooldown - delta);
 		MobTimer = Mathf.Max(0, MobTimer - delta);
-		
+
 		// HUD update
-		HUD.ChargeShotCooldownPercentage = 100 - (int)((CurrentChargeShotCooldown/CHARGE_SHOT_COOLDOWN)*100);
+		HUD.ChargeShotCooldownPercentage = 100 - (int)((CurrentChargeShotCooldown / CHARGE_SHOT_COOLDOWN) * 100);
 	}
-	
+
 	// Spawns Power Up, checks spawnchance.
-	private void SpawnPowerUp() {
-		int SpawnChance = rnd.Next(1,101);
-		if (SpawnChance <= PowerUpSpawnChance * 100) {
+	private void SpawnPowerUp()
+	{
+		int SpawnChance = rnd.Next(1, 101);
+		if (SpawnChance <= PowerUpSpawnChance * 100)
+		{
 			Vector2 Position = new Vector2(
-				rnd.Next(0,(int)ScreenSize.x),
-				rnd.Next(0,(int)ScreenSize.y)
+				rnd.Next(0, (int)ScreenSize.x),
+				rnd.Next(0, (int)ScreenSize.y)
 			);
-			int PowerUpType = rnd.Next(1,3);
+			int PowerUpType = rnd.Next(1, 3);
 			GD.Print(PowerUpType);
-			switch (PowerUpType) {
+			switch (PowerUpType)
+			{
 				case 1:
 					Milk milk = (Milk)MilkScene.Instance();
 					milk.Position = Position;
-					this.AddChildBelowNode(Background,milk);
+					this.AddChildBelowNode(Background, milk);
 					break;
 				case 2:
 					Catnip catnip = (Catnip)CatnipScene.Instance();
 					catnip.Position = Position;
-					this.AddChildBelowNode(Background,catnip);
+					this.AddChildBelowNode(Background, catnip);
 					break;
 			}
-		} else {
+		}
+		else
+		{
 			GD.Print("Unlucky! No Power-Up yet");
 		}
 	}
-	
-	private void SpawnMob() {
+
+	private void SpawnMob()
+	{
 		//Spawning Mobs in 4 locations
-		int location = rnd.Next(1,5);
-		int type = rnd.Next(1,101);
+		int location = rnd.Next(1, 5);
+		int type = rnd.Next(1, 101);
 		Area2D rat;
-		if (type <= BigRatSpawnChance*100) {
+		if (type <= BigRatSpawnChance * 100)
+		{
 			rat = (BigRat)BigRatScene.Instance();
-		} else {
+		}
+		else
+		{
 			rat = (Rat)RatScene.Instance();
 		}
-		switch(location) {
+		switch (location)
+		{
 			case 1:
 				rat.Position = LeftSpawnPosition.Position;
 				break;
@@ -170,58 +189,69 @@ public class Stage : Node
 			default:
 				break;
 		}
-		this.AddChildBelowNode(Background,rat);
+		this.AddChildBelowNode(Background, rat);
 		MobTimer = MOB_TIME; //TODO: Reduce time as time goes on
 	}
-	
+
 	//Get Bullet Velocity from inputs
-	private bool ShootFromMovement() {
+	private bool ShootFromMovement()
+	{
 		bool HasShot = false;
-		Bullet bullet = (Bullet)BulletScene.Instance();
+		Bullet bullet;
 		if (Input.IsActionPressed("shoot_up"))
 		{
+			bullet = (Bullet)BulletScene.Instance();
 			ShootUp(bullet);
 			HasShot = true;
 		}
 		if (Input.IsActionPressed("shoot_down"))
 		{
+			bullet = (Bullet)BulletScene.Instance();
 			ShootDown(bullet);
 			HasShot = true;
 		}
 		if (Input.IsActionPressed("shoot_left"))
 		{
+			bullet = (Bullet)BulletScene.Instance();
 			ShootLeft(bullet);
 			HasShot = true;
 		}
 		if (Input.IsActionPressed("shoot_right"))
 		{
+			bullet = (Bullet)BulletScene.Instance();
 			ShootRight(bullet);
 			HasShot = true;
 		}
 		return HasShot; // cannot use else case to allow for diagonals. 
 	}
-	private void Shoot(Bullet bullet) {
+	private void Shoot(Bullet bullet)
+	{
 		//GD.Print(bullet.velocity);
-		if (bullet.GetParent() == null && bullet.direction != Vector2.Zero) {
+		if (bullet.GetParent() == null && bullet.direction != Vector2.Zero)
+		{
 			AddChild(bullet);
 		}
 	}
-	private void ShootUp(Bullet bullet) {
+	private void ShootUp(Bullet bullet)
+	{
 		bullet.Position = player.Position;
 		bullet.direction.y -= 1;
 		Shoot(bullet);
 	}
-	private void ShootDown(Bullet bullet) {
+	private void ShootDown(Bullet bullet)
+	{
 		bullet.Position = player.Position;
 		bullet.direction.y += 1;
 		Shoot(bullet);
 	}
-	private void ShootLeft(Bullet bullet) {
+	private void ShootLeft(Bullet bullet)
+	{
 		bullet.Position = player.Position;
 		bullet.direction.x -= 1;
 		Shoot(bullet);
 	}
-	private void ShootRight(Bullet bullet) {
+	private void ShootRight(Bullet bullet)
+	{
 		bullet.Position = player.Position;
 		bullet.direction.x += 1;
 		Shoot(bullet);
